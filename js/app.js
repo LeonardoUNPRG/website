d67329d6a9763f0d835d3c64446c4e4f1abc7ad6
LeonardoUNPRG <97255466+LeonardoUNPRG@users.noreply.github.com>
2022-01-13 07:38:55 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let url=document.getElementById('imagen');
+function mostrar(valor){
+    switch(valor){
+        case 1: url.src = "imagenes/jose.jfif";
+            url.style.visibility = 'visible';
+            break;
+        case 2: url.src = "imagenes/smith.jfif";
+            url.style.visibility = 'visible';
+            break;
+        case 3: url.src = "imagenes/miguel.jpeg";
+            url.style.visibility = 'visible';
+            break;
+        case 4: url.src = "imagenes/leo.jfif";
+            url.style.visibility = 'visible';
+            break;
+        case 5: url.src = "imagenes/ionata.jfif";
+            url.style.visibility = 'visible';
+            break;
+        default:
+            break;
+    }
+}
+
